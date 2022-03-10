fea33e07f6de14541c963fc9bb873aa69590a985
d-qql <petrov.dmitrii@phystech.edu>
2022-03-11 00:27:46 +0300

    Initial commit

---
+++ test_package/test_build.cpp
@@ -0,0 +1,5 @@
+//
+// Created by d-qql on 11.03.2022.
+//
+#include <gtest/gtest.h>
+int main(){}
---
+++ tests/test_gtest.cpp
@@ -0,0 +1,7 @@
+//
+// Created by d-qql on 11.03.2022.
+//
+#include <gtest/gtest.h>
+TEST(TEST, GTEST){
+    std::cout << "TESTS IS OK" << std::endl;
+}
